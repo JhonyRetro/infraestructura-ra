a10646fa7ee554a3e25e2e4219b08075427eda9d
JhonyRetro <jhonan989@gmail.com>
2026-05-19 17:57:53 +0200

    grafana...

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ fi
 
 echo "[4/5] Levantando servicios con la nueva configuración..."
 systemctl start mosquitto
-nohup grafana-server > ./service/grafana.log
+su -c alumno nohup grafana-server > ./service/grafana.log
 systemctl start haproxy
 
 if systemctl is-active --quiet haproxy; then
